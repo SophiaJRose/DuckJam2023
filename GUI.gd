a0f12a963dbbda66171247734b0a51059c87b720
John Bell <2206568B@student.gla.ac.uk>
2023-02-26 08:41:00 +0000

    GUI and other changes

--- GUI.gd
+++ GUI.gd
@@ -1,6 +1,14 @@
 extends MarginContainer
 
 onready var timerLabel = get_node("VBoxContainer/TimerLabel")
+onready var timerBar = get_node("VBoxContainer/TimerBarBG/TimerBar")
 
 func _process(_delta):
-	timerLabel.text = "Timer: " + str(GlobalVariables.deathTimer)
+	timerLabel.text = ("%02d" % (GlobalVariables.survivalTimer / 3600)) + ":" + ("%02d" % ((GlobalVariables.survivalTimer / 60) % 60))
+	if GlobalVariables.deathTimer == -1:
+		timerBar.rect_scale.x = 1
+		timerBar.color = Color(0.875, 0.875, 0.875)
+	else:
+		var deathTimerPercent = float(GlobalVariables.deathTimer) / float(GlobalVariables.deathTimerMax)
+		timerBar.rect_scale.x = deathTimerPercent
+		timerBar.color = Color(0.875, clamp(1.75 * deathTimerPercent, 0, 0.875), clamp(1.75 * deathTimerPercent, 0, 0.875))
